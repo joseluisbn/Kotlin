5576d646f4463fe0fbc927c821e54d816635a28c
José Luis Blanco <joseluisbn@gmail.com>
2021-08-30 07:29:58 +0200

    Create mathclass.kt

---
+++ src/mathclass.kt
@@ -0,0 +1,13 @@
+import kotlin.math
+
+fun main() {
+    val num = 4.456
+    
+    //Redondeamos con el método round
+    
+    println(round(num))
+    
+    /*También podemos redondear:
+     * floor: hacia abajo
+     * ceil: hacia arriba*/
+}
