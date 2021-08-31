89ed07dd5e70d8af99db6b00ad9f2ea4e14c5088
José Luis Blanco <joseluisbn@gmail.com>
2021-08-31 22:48:30 +0200

    Create variables.kt

---
+++ src/variables.kt
@@ -0,0 +1,8 @@
+fun main() {
+    
+    // val (inmutable). No puede cambiar su valor una vez que ha sido asignado.
+    val nombre = "Barret"
+
+    //var (mutable). Su valor puede cambiar a lo largo del programa.
+    var telefono = 655842658
+}
