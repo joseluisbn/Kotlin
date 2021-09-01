b70442bbde0299b9e4a6e5a9441842fa316e46a7
José Luis Blanco <joseluisbn@gmail.com>
2021-09-01 17:45:15 +0200

    Create ifElse.kt

---
+++ src/ifElse.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    var login = false
+
+    if (login == true){
+        println("Bienvenido, usuari@")
+    } else{
+        println("Por favor, inicie sesión")
+    }
+
+    //También se puede expresar sin llaves cuando es una única línea de código.
+
+    if (login == true) println("Bienvenido, usuari@")
+    else println("Por favor, inicie sesión")
+}
