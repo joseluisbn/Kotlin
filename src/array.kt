de629f1cfb149e70d09b9321b19600748b1c9953
José Luis Blanco <joseluisbn@gmail.com>
2021-09-01 18:42:52 +0200

    Create array.kt

---
+++ src/array.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    //Creamos el array
+
+    var escritores = arrayOf("Frederik Pohl", "Liu Cixin", "Dan Simmons")
+
+    //Imprimimos el índice deseado
+
+    println(escritores[1])
+
+    //Podemos conocer el tamaño del array
+
+    println("El tamaño del array es " + escritores.size)
+
+    //Para reasignar el valor de una posición del array
+
+    escritores[0] = "Frank Herbert"
+
+    //Imprimimos y verificamos el cambio
+
+    println(escritores[0])
+
+}
